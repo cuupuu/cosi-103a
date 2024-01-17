b0ce39f2a8617638c7aa22a1e86801111a7a7015
Anny Lee <64579821+annybearlee@users.noreply.github.com>
2024-01-17 20:57:50 +0000

    Add handleClick function to Board

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -32,6 +32,11 @@ function Square({ value }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
