06340097a323fe259a25b76eb5ce6c25001e14e9
Anny Lee <64579821+annybearlee@users.noreply.github.com>
2024-01-17 21:16:15 +0000

    Update index.js

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -8,8 +8,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 
-import App from './App';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
