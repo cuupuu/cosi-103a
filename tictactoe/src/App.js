1fe5a0a7e8391833e27754beb024b0bfd836acfa
cuupuu <chupeiyan@brandeis.edu>
2024-01-17 16:35:53 -0500

    App.js -update

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import { useState } from 'react';
 
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
 
   return (
