6c901b9ab34ddd5f5b9d4228ade29c6491caa9c4
JaneXingLab <156869375+JaneXingLab@users.noreply.github.com>
2024-01-29 11:21:45 -0500

    Delete Sprint1/Sprint1_Restaurant directory

--- Sprint1/Sprint1_Restaurant/script.js
+++
@@ -1,4 +0,0 @@
-function showDetails(recipeNumber) {
-    const details = document.getElementById(`details${recipeNumber}`);
-    details.style.display = details.style.display === 'block' ? 'none' : 'block';
-}
