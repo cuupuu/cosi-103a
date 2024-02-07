b8948b56cdf41bdc86c629dbfa0862c8696eeabb
Anny Lee <annylee@AnnydeMBP.hsd1.ma.comcast.net>
2024-02-07 03:10:55 -0500

    sprint3 add nav bar

---
+++ mcdonalds2/src/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Contact() {
+    return (
+        <div className="contact">
+            <h1>Contact</h1>
+            <div className="boxes">
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
