461448aa045133fd07e5026e004310f0c40427a5
cuupuu <chupeiyan@brandeis.edu>
2024-02-07 16:26:19 -0500

    update docker

---
+++ Desktop/cosi-103a-sprint3_yenyilee/mcdonalds2/src/Contact.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const peopleData = [
+    {
+        name: "Ruixue Gou",
+        title: "Product Manager",
+        image: "path/to/jane.jpg",
+        description: "Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet."
+
+           },
+    {
+        name: "Yenyi Lee",
+        title: "Infrastructure Engineer",
+        image: "path/to/bob.jpg",
+        description: "Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet."
+   
+            },
+    {   
+        name: "Chupei Yan",
+        title: "Backend Developer",
+        image: "path/to/john.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero."
+
+        },
+    {
+        name: "Yuting Xing",
+        title: "Frontend Developer",
+        image: "path/to/alice.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero."
+    
+         }
+];
+
+function Contact() {
+    return (
+        <div className="contact">
+            <h1>Contact</h1>
+            <div className="boxes">
+                {peopleData.map((person, index) => (
+                    <div className="box" key={index}>
+                        <img src={person.image} alt={person.name} />
+                        <h2>{person.name}</h2>
+                        <h2>{person.title}</h2>
+                        <p>{person.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
