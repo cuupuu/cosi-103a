4d10e63db50e45613afb1f8fad3d48e31d1205f2
yuting <yutingxing@brandeis.edu>
2024-02-07 00:05:28 -0500

    update css and file name

--- mcdonalds2/src/App.js
+++ mcdonalds2/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import {Home} from './Landing_Page';
-import {RecipePage} from './Recipe_Page';
+import {Home} from './LandingPage';
+import {RecipePage} from './RecipePage';
 import recipes from './Recipe';
 
 function App() {
--- mcdonalds2/src/index.js
+++ mcdonalds2/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM   from 'react-dom/client';
 import App from './App.js';
 import {BrowserRouter} from 'react-router-dom';
-import './Landing_Page.css';
-import './Recipe_Page.css';
+import './LandingPage.css';
+import './RecipePage.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
