05bea712434b0bfea7ef280cd51d87b52d6a018b
annybearlee <bearlee0245@gmail.com>
2024-04-08 23:19:39 -0400

    Deleted chai, Used jest to test

--- frontend/src/test/testStability.test.js
+++ frontend/src/test/testStability.test.js
@@ -1,17 +1,28 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const request = require('supertest');
 const server = require('../index'); // adjust the path as needed
 
-chai.should();
-chai.use(chaiHttp);
-
 describe('Smoke Test', () => {
-  it('should return 200 for the /api/recipes endpoint', (done) => {
-    chai.request(server)
-      .get('/api/recipes')
-      .end((err, res) => {
-        res.should.have.status(200);
-        done();
-      });
+  it('should return 200 for the /api/recipes endpoint', async () => {
+    const response = await request(server).get('/api/recipes');
+    expect(response.statusCode).toBe(200);
   });
 });
+
+
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const server = require('../index'); // adjust the path as needed
+
+// chai.should();
+// chai.use(chaiHttp);
+
+// describe('Smoke Test', () => {
+//   it('should return 200 for the /api/recipes endpoint', (done) => {
+//     chai.request(server)
+//       .get('/api/recipes')
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+// });
